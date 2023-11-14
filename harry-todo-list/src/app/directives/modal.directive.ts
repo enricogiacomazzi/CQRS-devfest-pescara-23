5daa2d1bda0a60dc4cd60f0dfbed3e043402c2e1
Enrico <enrico@giacomazzi.cc>
2023-11-14 10:22:15 +0100

    cleanup modal directive

--- harry-todo-list/src/app/directives/modal.directive.ts
+++ harry-todo-list/src/app/directives/modal.directive.ts
@@ -4,26 +4,14 @@ import {Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges} from '@a
   selector: '[appModal]',
   standalone: true
 })
-export class ModalDirective implements OnInit, OnChanges {
-  @Input() appModal!: boolean
-  constructor(private el: ElementRef<HTMLDialogElement>) { }
-
-  ngOnInit(): void {
-    this.update();
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    this.update();
-  }
-
-  private update() {
-    if(!!this.el) {
-      if(this.appModal) {
-        this.el.nativeElement.showModal();
-      } else {
-        this.el.nativeElement.close();
-      }
+export class ModalDirective {
+  @Input() set appModal(value: boolean) {
+    if(value) {
+      this.el.nativeElement.showModal();
+    } else {
+      this.el.nativeElement.close();
     }
   }
+  constructor(private el: ElementRef<HTMLDialogElement>) {}
 
 }
